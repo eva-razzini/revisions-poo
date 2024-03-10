ffcfdb409e7940ae5f73b78bcaab1337613f87b3
unknown <eva.razzini@laplateforme.com>
2024-03-10 20:12:22 +0100

    add AbstractProduct

--- job13/index.php
+++ job13/index.php
@@ -98,7 +98,22 @@ class Category
         }
     }
 }
-class Product
+abstract class AbstractProduct
+{
+    // Déclarer les méthodes abstraites
+    abstract public function getId();
+    abstract public function getName();
+    abstract public function getPhotos();
+    abstract public function getPrice();
+    abstract public function getDescription();
+    abstract public function getQuantity();
+    abstract public function getCreatedAt();
+    abstract public function getUpdatedAt();
+    abstract public function getCategory_id();
+    
+    // Vous pouvez également déclarer d'autres méthodes abstraites si nécessaire
+}
+class Product extends AbstractProduct
 {
     private $id;
     private $name;
